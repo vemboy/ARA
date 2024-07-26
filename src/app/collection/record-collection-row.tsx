8158adf75ac154245f46c80fc09bc6a453912161
Vahakn Papazian <vahakn@MacBook-Pro-4.local>
2024-07-26 10:39:17 -0400

    lint

--- src/app/collection/record-collection-row.tsx
+++ src/app/collection/record-collection-row.tsx
@@ -1,43 +1,39 @@
 "use client";
 
 function RecordCollectionRow(props: any) {
-  console.log(props.display_title)
+  console.log(props.display_title);
   return (
     <>
       <div>
         {/* <div className="airtable-gallery-container"> */}
-        <div style={{backgroundColor: `${props.color}`}} className="div-container-container"> 
+        <div
+          style={{ backgroundColor: `${props.color}` }}
+          className="div-container-container"
+        >
           <div className="ARA_TITLE">ARA</div>
           <div className="play-button"></div>
-        <div className="div-container">
-        <a>
-          <img
-            src={`${props.src}`}
-            onClick={() => {
-              props.setCurrentSong(
-                `https://ara.directus.app/assets/${props.songId}`
-              ),
-                console.log(`https://ara.directus.app/assets/${props.songId}`),
-                console.log(props);
-            }}
-            className="airtable-gallery"
-          ></img>
-        </a>
-</div>
-
-        <div className="text-container">
-
-
-          <div>
-          {props.display_title}
-
+          <div className="div-container">
+            <a>
+              <img
+                src={`${props.src}`}
+                onClick={() => {
+                  props.setCurrentSong(
+                    `https://ara.directus.app/assets/${props.songId}`
+                  ),
+                    console.log(
+                      `https://ara.directus.app/assets/${props.songId}`
+                    ),
+                    console.log(props);
+                }}
+                className="airtable-gallery"
+              ></img>
+            </a>
           </div>
 
+          <div className="text-container">
+            <div>{props.display_title}</div>
+          </div>
         </div>
-        </div>
-
-
-
       </div>
     </>
   );
