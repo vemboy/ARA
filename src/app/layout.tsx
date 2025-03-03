a7e6709aaf666ed85623e45566be67726219b2fd
Vem <vem@papazian.info>
2025-03-03 16:11:25 -0500

    hello

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,12 @@
+import React from "react";
+import Script from "next/script";
+import { AudioLayout } from "./audioLayout";
+
+import "./globals.css";
+import "./globals2.css";
+import "./globals3.css";
+import "./globals4.css";
+
 export const dynamic = "force-dynamic";
 
 export const metadata = {
@@ -7,18 +16,16 @@ export const metadata = {
   },
 };
 
-import React from "react";
-import { AudioLayout } from "./audioLayout";
-
-import "./globals.css";
-import "./globals2.css";
-import "./globals3.css";
-import "./globals4.css";
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <AudioLayout>{children}</AudioLayout>;
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Script
+        data-goatcounter="https://ara.goatcounter.com/count"
+        async
+        src="//gc.zgo.at/count.js"
+        strategy="afterInteractive"
+      />
+      <AudioLayout>{children}</AudioLayout>
+    </>
+  );
 }
